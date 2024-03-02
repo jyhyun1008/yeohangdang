f210203a60a3bf600fd440693ed88468e02de25b
jyhyun1008 <hyun1008@kasi.re.kr>
2024-03-03 05:14:55 +0900

    as

--- js/index.js
+++ js/index.js
@@ -325,7 +325,8 @@ if (!page && !directory) {
     document.querySelector(".page_content").innerHTML += '<div class="article_list"></div>'
     var url = 'https://www.patreon.com/api/posts?filter[campaign_id]=9837321&include=attachments,user_defined_tags,campaign.rewards&field[post]=content,min_cents_pleged_to_view,post_type,title,url,published_at&sort=-published_at&filter[is_draft]=false'
     fetch(url, {
-        'mode': 'no-cors'
+        'mode': 'no-cors',
+        'content-type': 'application/jsonp',
     })
     .then(res => {
         console.log(res)
