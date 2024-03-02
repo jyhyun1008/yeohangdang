738df0159bc4bfb6691c57a76b602751c319da34
jyhyun1008 <hyun1008@kasi.re.kr>
2024-03-03 04:24:24 +0900

    asd

--- js/index.js
+++ js/index.js
@@ -327,7 +327,7 @@ if (!page && !directory) {
     fetch(url, {
         mode: 'no-cors'
     })
-    .then(res => res.text())
+    .then(res => res)
     .then((out) => {
         var result = JSON.parse(out)
         document.querySelector(".article_list").innerText += result
