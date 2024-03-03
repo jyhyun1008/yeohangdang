78cc9790e5ed99a5f274d82647fca7e516ce5bee
jyhyun1008 <hyun1008@kasi.re.kr>
2024-03-03 22:58:22 +0900

    아이콘 변경, 테마색 변경

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ const SITE_DESCRIPTION = "1인 복합 창작 문화생활공간"
 const SITE_FOOTER_HTML = "Copyright 2024. 여행당(YeoHangDang). All rights reserved."
 
 // 테마
-const ACCENT_COLOR = "#ff9899"
-const BG_COLOR = "#eee"
+const ACCENT_COLOR = "#ffdb77"
+const BG_COLOR = "#fafafa"
 
 // 깃허브 정보
 const USERNAME = "jyhyun1008"
